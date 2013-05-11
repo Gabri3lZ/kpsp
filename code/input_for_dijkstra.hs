4aefec7312f1b4b9f95f2b4fd8d573c4df28abfa
Reto Lehnherr <reto.lehnherr@mac.com>
2013-05-11 14:37:46 +0200

    finde nächste node

--- code/input_for_dijkstra.hs
+++ code/input_for_dijkstra.hs
@@ -14,11 +14,22 @@ resultExpected = [("a", 0, []),
 					("e", 4, ["a", "b"]),
 					("f", 5, ["a", "b", "e"])]
 
+-- Initialisiere die Routes Tabelle anhand des Graphen
 setupRoute ((a,_):graph) = (a,0,[]):[(fst g, -1, []) | g <- graph]
+
+-- Route mit kleinster Metrik
+minRoute (routeIdL, weightL) (routeIdR,weightR,_) = if weightL < weightR then (routeIdL, weightL) else (routeIdR, weightR)
+
+--- Node mit kleinstem Gewicht
+nextNode :: [([Char], t)] -> [([Char], Int, t1)] -> [Char]
+nextNode graph routes = nextNodeId
+	where
+		graphIds = [nodeId | (nodeId,_) <- graph] 
+		possibleRoutes = [(routeId, weight, route) | (routeId, weight, route) <- routes, weight >= 0, elem routeId graphIds] 
+		nextNodeId = fst (foldl minRoute ("routeId", 1000) possibleRoutes)
 
 
 dijkstra :: [([Char], [([Char], Integer)])] -> [([Char], Integer, [[Char]])] -> [([Char], Integer, [[Char]])] 
 dijkstra graph [] = dijkstra graph (setupRoute graph) -- einstieg
 dijkstra [] routes = routes -- basisfall
 dijkstra graph routes = dijkstra [] routes -- rekursionsfall
-
