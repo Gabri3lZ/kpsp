e25cf5960abe8893ac70e1c4eee871d8b844d88b
Reto Lehnherr <reto.lehnherr@mac.com>
2013-05-30 11:42:07 +0200

    jetzt and io

--- code/ospf_dijkstra.hs
+++ code/ospf_dijkstra.hs
@@ -1,4 +1,5 @@
 import Prelude
+import System.IO
 
 -- Typendeklaration: Bessere lesbarkeit
 
@@ -46,21 +47,21 @@ metricOffset = 10
 metricLength = 2
 
 
-
--- Nimt einen Hex String entgegen und gibt eine Integer Räpresentation als String zurück
---hexStringToIntString :: [Char] -> [Char]
+-- |Nimt einen Hex String entgegen und gibt eine Integer Räpresentation als String zurück
+hexStringToIntString :: String -> RouterId
 --hexStringToInt string =
 
--- Liest die Nachbarschaftstabelle aus einem File ein und gibt eine Liste mit Nachbarn und deren Eigenschaften zurück
---readNeighbourTable :: [Char] -> [([Char], [Char], [Char], Integer, Integer)]
+-- |Liest die Nachbarschaftstabelle aus einem File ein und gibt eine Liste mit Nachbarn und deren Eigenschaften zurück
+readNeighbourTable :: FilePath -> [(Address, Interface, State, Priority, Dead)]
 
--- Liest ein LinkStateUpdate aus einem File ein und gibt die Topologie Tabelle als Liste zurück
---readLinkStateUpdate :: [Char] -> [([Char], [([Char], Integer)]]
+-- |Liest ein LinkStateUpdate aus einem File ein und gibt die Topologie Tabelle (Graph) als Liste zurück
+readLinkStateUpdate :: FilePath -> Graph
 --readLinkStateUpdate filePath = do
 --	update <- readFile "linkstateupdates/linkstateupdate1.txt"
 
 
--- der graph wie er erstellt werden sollte
+
+-- |Der graph wie er erstellt werden sollte
 graphInput = [(1, [(2, 2), (3, 100), (4, 20)]),
 			(2, [(5, 2), (6, 50), (1, 2)]),
 			(3, [(4, 1), (6, 5), (1, 100)]),
@@ -68,8 +69,8 @@ graphInput = [(1, [(2, 2), (3, 100), (4, 20)]),
 			(5, [(2, 2), (6, 1)]),
 			(6, [(4, 10), (3, 5), (2, 50), (5, 1)])]
 
--- Das Resultat, das wir erwarten
-resultExpected :: [(RouterId, Metric, Route)]
+-- |Das Resultat, das wir erwarten
+resultExpected :: [ShortestPath]
 resultExpected = [(1, 0, []),
 					(2, 2, [1]),
 					(3, 10, [1, 2, 5, 6]),
@@ -77,13 +78,15 @@ resultExpected = [(1, 0, []),
 					(5, 4, [1, 2]),
 					(6, 5, [1, 2, 5])]
 
--- Initialisiere die Routes Tabelle anhand des Graphen
+-- |Initialisiere die Routes Tabelle anhand des Graphen
+setupRoute :: Graph -> [ShortestPath]
 setupRoute ((a,_):graph) = (a,0,[]):[(fst g, -1, []) | g <- graph]
 
--- Route mit kleinster Metrik
+-- |Finde Route mit kleinster Metrik
+minRoute :: (RouterId, Metric) -> ShortestPath -> (RouterId, Metric)
 minRoute (routeIdL, weightL) (routeIdR,weightR,_) = if weightL < weightR then (routeIdL, weightL) else (routeIdR, weightR)
 
--- NodeId mit kleinstem Gewicht
+-- |Gibt RouterId zurück, welcher noch nicht markiert ist und den aktuell kürzesten Pfad hat
 nextNode :: Graph -> [ShortestPath] -> RouterId
 nextNode graph routes = nextNodeId
 	where
@@ -91,28 +94,31 @@ nextNode graph routes = nextNodeId
 		possibleRoutes = [(routeId, weight, route) | (routeId, weight, route) <- routes, weight >= 0, elem routeId graphIds]
 		nextNodeId = fst (foldl minRoute (99, 10000) possibleRoutes)
 
-
-refreshRoute ((neighbourId, neibourDistance):ns) currentRoute routes = refreshRoute ns currentRoute updated
+updateShortestPaths :: [(RouterId, Distance)] -> ShortestPath -> [ShortestPath] -> [ShortestPath]
+updateShortestPaths ((neighbourId, neibourDistance):ns) currentRoute routes = updateShortestPaths ns currentRoute updated
 	where
 		(currentNodeId, currentNodeMetrik, currentNodeRoute) = currentRoute
 		updated = [if (metrik == -1 || currentNodeMetrik + neibourDistance < metrik) && neighbourId == nodeId
 			then (nodeId, currentNodeMetrik + neibourDistance, currentNodeRoute ++ [currentNodeId])
 			else (nodeId, metrik, route) | (nodeId, metrik, route) <- routes]
-refreshRoute [] _ routes = routes
+updateShortestPaths [] _ routes = routes
 
--- find current route to a node
+-- |Findet aktuellen kürzesten Pfad zu einem Router
+currentRoute :: [ShortestPath] -> RouterId -> ShortestPath
 currentRoute routes targetNode = head [(nodeId, metric, route) | (nodeId, metric, route) <- routes, nodeId == targetNode]
 
 
--- Berechung der kürzesten Pfade. Nimt Topologie Tabelle als input Parameter und gibt eine Liste von Trippeln (ID, Metrik, Route) zurück
-dijkstra :: [(RouterId, [(RouterId, Distance)])] -> [ShortestPath] -> [ShortestPath]
+-- |Berechung der kürzesten Pfade. Nimt Topologie Tabelle als input Parameter und gibt eine Liste von Trippeln (ID, Metrik, Route) zurück
+dijkstra :: Graph -> [ShortestPath] -> [ShortestPath]
 dijkstra graph [] = dijkstra graph (setupRoute graph) -- einstieg: erstelle initiale Route tabledijkstra [] routes = routes -- basisfall: Routentabelle erstellt
 dijkstra [] routes = routes -- basisfall: Routentabelle erstellt
-dijkstra graph routes = dijkstra restGraph refreshRoutes  --- 
+dijkstra graph routes = dijkstra restGraph updateShortestPathss  --- 
 	where
 		nextNodeId = nextNode graph routes
 		currentNode = head [(nodeId, neighbours) | (nodeId, neighbours) <- graph, nodeId == nextNodeId]
 		restGraph = [(nodeId, neighbours) | (nodeId, neighbours) <- graph, nodeId /= nextNodeId]			
 		(_, neighbours) = currentNode
-		refreshRoutes = refreshRoute neighbours (currentRoute routes nextNodeId) routes
+		updateShortestPathss = updateShortestPaths neighbours (currentRoute routes nextNodeId) routes
 
+main = do 
+		print (dijkstra graphInput []) == resultExpected)
